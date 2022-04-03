fb2328697f3a44f1daa66e835e7f42dfe0ea0cdf
Makiv-Vik <viktorkaktysikov@gmail.com>
2022-04-03 19:33:12 +0300

    add authorization guard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from 'src/auth/auth.module';
 import { Role } from 'src/role/role.model';
 import { RoleModule } from 'src/role/role.module';
 import { Team } from 'src/team/team.model';
@@ -14,6 +15,7 @@ import { UserService } from './user.service';
   imports: [
     SequelizeModule.forFeature([User, Role, Team, UserTeam]),
     RoleModule,
+    forwardRef(() => AuthModule),
   ],
   exports: [UserService],
 })
